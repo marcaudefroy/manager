5a17afb2561e3f505548b87fc65d47e95b4770ee
Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
2022-07-15 10:11:48 +0200

    fix(dedicated.server): add banner firewall cisco asa eol
    ref: MANAGER-9617 MANAGER-9717
    
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>

--- packages/manager/apps/dedicated/client/app/dedicated/server/details/server.controller.js
+++ packages/manager/apps/dedicated/client/app/dedicated/server/details/server.controller.js
@@ -7,6 +7,7 @@ import isEmpty from 'lodash/isEmpty';
 import set from 'lodash/set';
 
 import {
+  GUIDES_FIREWALL_CISCO_ASA_EOL,
   NO_AUTORENEW_COUNTRIES,
   BYOI_STARTING_MESSAGE,
   BYOI_STATUS_ENUM,
@@ -28,6 +29,7 @@ export default class ServerCtrl {
     Poller,
     Polling,
     Server,
+    ServerFirewallAsa,
     User,
   ) {
     this.$q = $q;
@@ -43,6 +45,7 @@ export default class ServerCtrl {
     this.Poller = Poller;
     this.Polling = Polling;
     this.Server = Server;
+    this.ServerFirewallAsa = ServerFirewallAsa;
     this.User = User;
   }
 
@@ -352,7 +355,11 @@ export default class ServerCtrl {
       this.launchBringYourOwnImagePolling();
     }
 
-    return this.$q.all([this.load(), this.fetchRTMInfo()]);
+    return this.$q.all([
+      this.load(),
+      this.fetchRTMInfo(),
+      this.firewallAsaInformations(),
+    ]);
   }
 
   load() {
@@ -675,6 +682,21 @@ export default class ServerCtrl {
         null,
         'rtm_eol_info',
       );
+    });
+  }
+
+  firewallAsaInformations() {
+    return this.ServerFirewallAsa.getInformations(
+      this.$stateParams.productId,
+    ).then((informations) => {
+      if (informations.enabled) {
+        this.firewallAsaIsEnabled = true;
+        this.User.getUser().then(({ language }) => {
+          this.linkFirewallCiscoAsa =
+            GUIDES_FIREWALL_CISCO_ASA_EOL[language] ||
+            GUIDES_FIREWALL_CISCO_ASA_EOL.default;
+        });
+      }
     });
   }
 }

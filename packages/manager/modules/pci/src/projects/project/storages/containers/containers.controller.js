bd86decc838f7014cc6099578f95063eafba055e
sachinrameshn <sachin.ramesh@ovhcloud.com>
2022-07-21 15:23:16 +0530

    fix(storages-container): add toggle button for Swift container
     ref: MANAGER-9729
     add toggle button between public and private siwft conatiners
    
    Signed-off-by: sachinrameshn <sachin.ramesh@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/storages/containers/containers.controller.js
+++ packages/manager/modules/pci/src/projects/project/storages/containers/containers.controller.js
@@ -20,6 +20,37 @@ export default class PciStoragesContainersController {
     this.hasHighPerformanceStorage = this.hasHighPerformanceStorage();
   }
 
+  onPublicToggle(container) {
+    this.loadingContainer = container.id;
+    return this.$http
+      .put(`/cloud/project/${this.projectId}/storage/${container.id}`, {
+        containerType: container.state ? 'private' : 'public',
+      })
+      .then(() =>
+        this.CucCloudMessage.success(
+          this.$translate.instant(
+            container.state
+              ? 'pci_projects_project_storages_containers_toggle_private_succeed'
+              : 'pci_projects_project_storages_containers_toggle_public_succeed',
+            { name: container.name },
+          ),
+          'pci.projects.project.storages.containers.container',
+        ),
+      )
+      .catch((error) =>
+        this.CucCloudMessage.error(
+          this.$translate.instant(
+            'pci_projects_project_storages_containers_toggle_fail',
+            { message: error.data?.message },
+          ),
+          'pci.projects.project.storages.containers.container',
+        ),
+      )
+      .finally(() => {
+        this.loadingContainer = null;
+      });
+  }
+
   loadMessages() {
     this.messageHandler = this.CucCloudMessage.subscribe(
       'pci.projects.project.storages.containers',
